c83a36bee4cac4842ac08037abccef3603d79232
unknown <rupe.md@gmail.com>
2021-06-01 18:00:04 +0300

    fixed

---
+++ src/data/samurays.js
@@ -0,0 +1,47 @@
+export const samurays = [
+  {
+    id: 1,
+    name: "notatnik śmierci",
+    descr:
+      "Główny bohater anime, Light Yagami, jest najlepszym licealistą w Japonii i synem policjanta. Pewnego dnia bóg śmierci Ryuk znudził się, więc postanowił wrzucić jeden ze swoich zapisów śmierci do świata ludzi. W drodze ze szkoły Light znajduje leżący na ziemi zeszyt i postanawia przetestować jego działanie. Wracając do domu, zgodnie z regulaminem, wpisał imię przestępcy, który był wtedy pokazywany w telewizji. Ku zdziwieniu Lighta, sprawca rzeczywiście zmarł na atak serca czterdzieści sekund później. Upewniwszy się, że akt śmierci działa, postanawia zbudować nowy świat, w którym będą żyć tylko dobrzy i odpowiedzialni ludzie, odbierając życie wszystkim przestępcom wspomnianym w mediach . Po chwili Ryuk pojawia się Lightowi. Mówi, że Light otrzymał notatnik przez czysty przypadek, ponieważ Ryuk wysłał go do świata ludzi z nudów, a teraz ma nadzieję, że Light go zabawi, ale nie obiecuje pomocy, w większości pozostając jedynie zewnętrznym obserwatorem.",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/ru/a/a9/Death_Note_Manga_Cover_Russian.jpg",
+    tags: ["notatnik_śmierci"],
+  },
+  {
+    id: 2,
+    name: "tokyo ghoul",
+    descr:
+      "Ken Kaneki to student uniwersytetu, który w wyniku ataku ghula trafia do szpitala, gdzie nielegalnie przeszczepia narządy tego samego i jednego z najsilniejszych ghuli, aby uratować mu życie. Aby przetrwać, ghule muszą żywić się ludzkim mięsem, więc zabijają ludzi lub znajdują ciała samobójców. Z powodu takiego przeszczepu narządu Kaneki staje się tylko pół ghulem, ale musi odżywiać się ludzkim mięsem, jak wszyscy inni. Kaneki stara się zachować swoje człowieczeństwo, próbując pozostać w kontakcie ze światem ludzi, zanurzając się w społeczności ghuli.",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg",
+    tags: ["tokyo_ghoul"],
+  },
+  {
+    id: 3,
+    name: "Bardzo miło , Bóg",
+    descr:
+      "Ojciec Momodzono, Nanami, wyjechał w nieznanym kierunku, z powodu dużej sumy pieniędzy. Następnie Nanami spotyka Mikage, dziwnego mężczyznę w okularach, którego przeraża pies. Nanami „ratuje” go przed psem i opowiada mu jego historię. W dowód wdzięczności za jej zbawienie pozwala jej mieszkać w swoim domu. W przypadku braku własnego domu Nanami jest zmuszona się zgodzić. Mikage daje jej „mapę”, która prowadzi ją do świątyni. Asystenci świątyni Onikiri i Kotetsu witają Momodzono jako nową Boginię Świątyni Ziemi, po czym dziewczyna spotyka innego demona - Tomoe the Guardian. Jednak Tomoe, nie rozpoznając Nanami, opuszcza świątynię, ale później zawiera umowę z dziewczyną i nadal zostaje jej opiekunem. Wkrótce Nanami zakochuje się w Tomoe, myląc jego lojalność z miłością. Później sam Tomoe zakochuje się w Momodzono.",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/ru/8/85/Kamisama_Hajimemashita.jpg",
+    tags: ["Bardzo_miło_Bóg"],
+  },
+  {
+    id: 4,
+    name: "The seven deadly sins",
+    descr:
+      "Dziesięć lat przed głównymi wydarzeniami następuje wielkie nieszczęście: Lwice atakują stolicę królestwa, zabijając prawie wszystkich Świętych Rycerzy, a siedmiu najsilniejszych rycerzy - siedem grzechów głównych - zostaje uznanych za winnych. Pogłoski o ich skrajnym okrucieństwie i okrucieństwie zaczynają krążyć wśród ludzi. Dziś królestwo przechodzi zamach stanu, gdy Święci Rycerze przejmują władzę i więzią rodzinę królewską. Młoda księżniczka Elżbieta udaje się uciec i wyrusza w długą podróż w poszukiwaniu grzechów śmiertelnych, widząc w nich ostatnią nadzieję na zbawienie rodziny królewskiej i całej Lwicy. Próbując pozbyć się prześladowców, dziewczyna wędruje do tawerny młodego mężczyzny, który faktycznie okazuje się byłym przywódcą Grzechów Głównych - Meliodasem, który jest właścicielem najsilniejszego z siedmiu grzechów - smoka. grzech gniewu. Słyszy prośbę dziewczyny i zgadza się pomóc jej znaleźć resztkę jej grzechów.",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/ru/thumb/2/2a/Nanatsu_no_Taizai1.png/345px-Nanatsu_no_Taizai1.png",
+    tags: ["The_seven_deadly_sins"],
+  },
+  {
+    id: 5,
+    name: "Black Butler",
+    descr:
+      "Akcja rozgrywa się w wiktoriańskiej Anglii. Głównym bohaterem jest hrabia Ciel Phantomhive. Do jego obowiązków należy ochrona Jej Wysokości, walka z mafią. W wyniku ugody z demonem, zgodnie z którą po wypełnieniu warunków kontraktu zabierze swoją duszę, otrzymał idealnego lokaja i strażnika. Sebastian spełnia każde polecenie swojego pana i musi chronić Ciela przed śmiercią, dopóki nie spełni warunków umowy.",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/ru/2/27/Kuroshitsuji_Volume_1_cover.jpg",
+    tags: ["Black_Butler"],
+  },
+];
